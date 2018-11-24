81ed45bb47d91c1e6edab81240a9dbd61d77d9e2
Joey Boeme <joey.boeme@hotmail.fr>
2018-11-24 18:38:38 +0100

    RecipeController => init

---
+++ DessertTaCeinture.API/DessertTaCeinture.API/Controllers/RecipeController.cs
@@ -0,0 +1,43 @@
+﻿using DessertTaCeinture.DAL.Entities;
+using DessertTaCeinture.DAL.UnitOfWork;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace DessertTaCeinture.API.Controllers
+{
+    public class RecipeController : ApiController
+    {
+        private readonly UnitOfWork UOW = new UnitOfWork();
+
+        [HttpGet]
+        public IEnumerable<RecipeEntity> Get()
+        {
+            return UOW.RecipeRepository.GetEntities();
+        }
+
+        [HttpGet]
+        public RecipeEntity Get(int id)
+        {
+            return UOW.RecipeRepository.GetEntity(id);
+        }
+
+        [HttpPost]
+        public bool Post(RecipeEntity entity)
+        {
+            return (UOW.RecipeRepository.AddEntity(entity));
+        }
+
+        [HttpPut]
+        public bool Put(int id, RecipeEntity entity)
+        {
+            if (id.Equals(entity.Id)) return (UOW.RecipeRepository.UpdateEntity(entity));
+            else return false;
+        }
+
+        [HttpDelete]
+        public bool Delete(int id)
+        {
+            return (UOW.RecipeRepository.DeleteEntity(id));
+        }
+    }
+}
