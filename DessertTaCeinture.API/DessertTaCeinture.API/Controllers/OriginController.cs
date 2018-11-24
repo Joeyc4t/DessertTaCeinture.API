04f3d71d9208ad734e6176fb9a0a735714547786
Joey Boeme <joey.boeme@hotmail.fr>
2018-11-24 18:25:50 +0100

    OriginController => init

---
+++ DessertTaCeinture.API/DessertTaCeinture.API/Controllers/OriginController.cs
@@ -0,0 +1,43 @@
+﻿using DessertTaCeinture.DAL.Entities;
+using DessertTaCeinture.DAL.UnitOfWork;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace DessertTaCeinture.API.Controllers
+{
+    public class OriginController : ApiController
+    {
+        private readonly UnitOfWork UOW = new UnitOfWork();
+
+        [HttpGet]
+        public IEnumerable<OriginEntity> Get()
+        {
+            return UOW.OriginRepository.GetEntities();
+        }
+
+        [HttpGet]
+        public OriginEntity Get(int id)
+        {
+            return UOW.OriginRepository.GetEntity(id);
+        }
+
+        [HttpPost]
+        public bool Post(OriginEntity entity)
+        {
+            return (UOW.OriginRepository.AddEntity(entity));
+        }
+
+        [HttpPut]
+        public bool Put(int id, OriginEntity entity)
+        {
+            if (id.Equals(entity.Id)) return (UOW.OriginRepository.UpdateEntity(entity));
+            else return false;
+        }
+
+        [HttpDelete]
+        public bool Delete(int id)
+        {
+            return (UOW.OriginRepository.DeleteEntity(id));
+        }
+    }
+}
