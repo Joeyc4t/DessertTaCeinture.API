f6fd82445daccff5b17e1d7d26a52d3b3add2910
Joey Boeme <joey.boeme@hotmail.fr>
2018-11-24 18:28:09 +0100

    PictureController => init

---
+++ DessertTaCeinture.API/DessertTaCeinture.API/Controllers/PictureController.cs
@@ -0,0 +1,43 @@
+﻿using DessertTaCeinture.DAL.Entities;
+using DessertTaCeinture.DAL.UnitOfWork;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace DessertTaCeinture.API.Controllers
+{
+    public class PictureController : ApiController
+    {
+        private readonly UnitOfWork UOW = new UnitOfWork();
+
+        [HttpGet]
+        public IEnumerable<PictureEntity> Get()
+        {
+            return UOW.PictureRepository.GetEntities();
+        }
+
+        [HttpGet]
+        public PictureEntity Get(int id)
+        {
+            return UOW.PictureRepository.GetEntity(id);
+        }
+
+        [HttpPost]
+        public bool Post(PictureEntity entity)
+        {
+            return (UOW.PictureRepository.AddEntity(entity));
+        }
+
+        [HttpPut]
+        public bool Put(int id, PictureEntity entity)
+        {
+            if (id.Equals(entity.Id)) return (UOW.PictureRepository.UpdateEntity(entity));
+            else return false;
+        }
+
+        [HttpDelete]
+        public bool Delete(int id)
+        {
+            return (UOW.PictureRepository.DeleteEntity(id));
+        }
+    }
+}
