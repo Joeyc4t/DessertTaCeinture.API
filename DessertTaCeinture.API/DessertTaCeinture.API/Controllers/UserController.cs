a473fa0479edc3c757924e3e789136f78122f261
Joey Boeme <joey.boeme@hotmail.fr>
2018-11-24 17:52:19 +0100

    UserController => init

---
+++ DessertTaCeinture.API/DessertTaCeinture.API/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using DessertTaCeinture.DAL.Entities;
+using DessertTaCeinture.DAL.UnitOfWork;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace DessertTaCeinture.API.Controllers
+{
+    public class UserController : ApiController
+    {
+        private readonly UnitOfWork UOW = new UnitOfWork(); 
+
+        public IEnumerable<UserEntity> Get()
+        {
+            return UOW.UserRepository.GetEntities();
+        }
+
+        public UserEntity Get(int id)
+        {
+            return UOW.UserRepository.GetEntity(id);
+        }
+
+        public bool Post(UserEntity entity)
+        {
+            return (UOW.UserRepository.AddEntity(entity));
+        }
+
+        public bool Put(int id, UserEntity entity)
+        {
+            if (id.Equals(entity.Id)) return (UOW.UserRepository.UpdateEntity(entity));
+            else return false;
+        }
+
+        public bool Delete(int id)
+        {
+            return (UOW.UserRepository.DeleteEntity(id));
+        }
+    }
+}
