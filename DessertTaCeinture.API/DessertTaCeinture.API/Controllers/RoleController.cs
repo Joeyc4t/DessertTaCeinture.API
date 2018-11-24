77937b41b586cf8614983c48056bf9d68b25ca73
Joey Boeme <joey.boeme@hotmail.fr>
2018-11-24 18:40:03 +0100

    RoleController => init

---
+++ DessertTaCeinture.API/DessertTaCeinture.API/Controllers/RoleController.cs
@@ -0,0 +1,43 @@
+﻿using DessertTaCeinture.DAL.Entities;
+using DessertTaCeinture.DAL.UnitOfWork;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace DessertTaCeinture.API.Controllers
+{
+    public class RoleController : ApiController
+    {
+        private readonly UnitOfWork UOW = new UnitOfWork();
+
+        [HttpGet]
+        public IEnumerable<RoleEntity> Get()
+        {
+            return UOW.RoleRepository.GetEntities();
+        }
+
+        [HttpGet]
+        public RoleEntity Get(int id)
+        {
+            return UOW.RoleRepository.GetEntity(id);
+        }
+
+        [HttpPost]
+        public bool Post(RoleEntity entity)
+        {
+            return (UOW.RoleRepository.AddEntity(entity));
+        }
+
+        [HttpPut]
+        public bool Put(int id, RoleEntity entity)
+        {
+            if (id.Equals(entity.Id)) return (UOW.RoleRepository.UpdateEntity(entity));
+            else return false;
+        }
+
+        [HttpDelete]
+        public bool Delete(int id)
+        {
+            return (UOW.RoleRepository.DeleteEntity(id));
+        }
+    }
+}
