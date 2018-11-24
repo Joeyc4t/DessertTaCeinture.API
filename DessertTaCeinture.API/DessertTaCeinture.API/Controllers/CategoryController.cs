d5464467666fd01164106253870f5c28516add7f
Joey Boeme <joey.boeme@hotmail.fr>
2018-11-24 18:21:25 +0100

    CategoryController => init

---
+++ DessertTaCeinture.API/DessertTaCeinture.API/Controllers/CategoryController.cs
@@ -0,0 +1,47 @@
+﻿using DessertTaCeinture.DAL.Entities;
+using DessertTaCeinture.DAL.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DessertTaCeinture.API.Controllers
+{
+    public class CategoryController : ApiController
+    {
+        private readonly UnitOfWork UOW = new UnitOfWork();
+
+        [HttpGet]
+        public IEnumerable<CategoryEntity> Get()
+        {
+            return UOW.CategoryRepository.GetEntities();
+        }
+
+        [HttpGet]
+        public CategoryEntity Get(int id)
+        {
+            return UOW.CategoryRepository.GetEntity(id);
+        }
+
+        [HttpPost]
+        public bool Post(CategoryEntity entity)
+        {
+            return (UOW.CategoryRepository.AddEntity(entity));
+        }
+
+        [HttpPut]
+        public bool Put(int id, CategoryEntity entity)
+        {
+            if (id.Equals(entity.Id)) return (UOW.CategoryRepository.UpdateEntity(entity));
+            else return false;
+        }
+
+        [HttpDelete]
+        public bool Delete(int id)
+        {
+            return (UOW.CategoryRepository.DeleteEntity(id));
+        }
+    }
+}
--- DessertTaCeinture.API/DessertTaCeinture.API/Controllers/UserController.cs
+++ DessertTaCeinture.API/DessertTaCeinture.API/Controllers/UserController.cs
@@ -9,27 +9,32 @@ namespace DessertTaCeinture.API.Controllers
     {
         private readonly UnitOfWork UOW = new UnitOfWork(); 
 
+        [HttpGet]
         public IEnumerable<UserEntity> Get()
         {
             return UOW.UserRepository.GetEntities();
         }
 
+        [HttpGet]
         public UserEntity Get(int id)
         {
             return UOW.UserRepository.GetEntity(id);
         }
 
+        [HttpPost]
         public bool Post(UserEntity entity)
         {
             return (UOW.UserRepository.AddEntity(entity));
         }
 
+        [HttpPut]
         public bool Put(int id, UserEntity entity)
         {
             if (id.Equals(entity.Id)) return (UOW.UserRepository.UpdateEntity(entity));
             else return false;
         }
 
+        [HttpDelete]
         public bool Delete(int id)
         {
             return (UOW.UserRepository.DeleteEntity(id));
