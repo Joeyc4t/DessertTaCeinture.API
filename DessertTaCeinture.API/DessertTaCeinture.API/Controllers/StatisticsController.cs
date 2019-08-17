4cf5a57cda399c68691cc202298acb2a2d9b6f56
Joey Boeme <joey.boeme@hotmail.fr>
2019-08-17 16:42:01 +0200

    Statistics management

---
+++ DessertTaCeinture.API/DessertTaCeinture.API/Controllers/StatisticsController.cs
@@ -0,0 +1,47 @@
+﻿using DessertTaCeinture.DAL.Repositories;
+using System.Collections.Generic;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace DessertTaCeinture.API.Controllers
+{
+    /// <summary>
+    /// Statistics controller.
+    /// </summary>
+    public class StatisticsController : ApiController
+    {
+        private StatisticsRepository repo = new StatisticsRepository();
+
+        [HttpGet]
+        [Route("api/Statistics/GenerateChart")]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public Dictionary<string, int> GenerateChart(string id)
+        {
+            Dictionary<string, int> dico = new Dictionary<string, int>();
+
+            switch (id)
+            {
+                case "chartUsers":
+                    dico = repo.GetUsersCount();
+                    break;
+                case "chartCommentariesCount":
+                    dico = repo.GetCommentariesCount();
+                    break;
+                case "chartRecipesByCategory":
+                    dico = repo.GetRecipesByCategoryCount();
+                    break;
+                case "chartRecipesByOrigineCount":
+                    dico = repo.GetRecipesByOriginCount();
+                    break;
+                case "chartRecipesVisibility":
+                    dico = repo.GetRecipesCount();
+                    break;
+                case "chartRates":
+                    dico = repo.GetRatesCount();
+                    break;
+            };
+
+            return dico;
+        }
+    }
+}
---
+++ DessertTaCeinture.DAL/DessertTaCeinture.DAL/Repositories/StatisticsRepository.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace DessertTaCeinture.DAL.Repositories
+{
+    public class StatisticsRepository
+    {
+        private string connString = ConfigurationManager.AppSettings["connString"];
+
+        public Dictionary<string, int> GetUsersCount()
+        {
+            string query = @"SELECT CASE IsActive WHEN 1 THEN 'Actif'
+					                              WHEN 0 THEN 'Inactif' END AS 'Key', COUNT(Id) AS 'Value'
+                             FROM [User]
+                             GROUP BY IsActive";
+            return GenerateDictionary(query);
+        }
+
+        public Dictionary<string, int> GetCommentariesCount()
+        {
+            string query = @"SELECT 'Commentaires' AS 'Key', COUNT(Id) AS 'Value'
+                             FROM Rate
+                             WHERE Commentary IS NOT NULL";
+            return GenerateDictionary(query);
+        }
+
+        public Dictionary<string, int> GetRecipesByCategoryCount()
+        {
+            string query = @"SELECT c.Name AS 'Key', COUNT(r.Id) AS 'Value'
+                             FROM Category c
+                             LEFT JOIN Recipe r ON r.CategoryId = c.Id
+                             GROUP BY c.Name";
+            return GenerateDictionary(query);
+        }
+
+        public Dictionary<string, int> GetRecipesByOriginCount()
+        {
+            string query = @"SELECT o.Country AS 'Key', COUNT(r.Id) AS 'Value'
+                             FROM Origin o
+                             LEFT JOIN Recipe r ON r.OriginId = o.Id
+                             GROUP BY o.Country";
+            return GenerateDictionary(query);
+        }
+
+        public Dictionary<string, int> GetRecipesCount()
+        {
+            string query = @"SELECT CASE IsPublic WHEN 1 THEN 'Publique'
+					                              WHEN 0 THEN 'Privée' END AS 'Key', COUNT(Id) AS 'Value'
+                             FROM Recipe
+                             GROUP BY IsPublic";
+            return GenerateDictionary(query);
+        }
+
+        public Dictionary<string, int> GetRatesCount()
+        {
+            string query = @"SELECT 'Cotations' AS 'Key', COUNT(Id) AS 'Value'
+                             FROM Rate";
+            return GenerateDictionary(query);
+        }
+
+        private Dictionary<string, int> GenerateDictionary(string query)
+        {
+            Dictionary<string, int> dico = new Dictionary<string, int>();
+
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+
+                using (SqlCommand command = new SqlCommand(query, conn))
+                {
+                    using (SqlDataReader rdr = command.ExecuteReader())
+                    {
+                        while (rdr.Read())
+                        {
+                            dico.Add(rdr.GetString(0), rdr.GetInt32(1));
+                        }
+                    }
+                }
+            }
+
+            return dico;
+        }
+    }
+}
